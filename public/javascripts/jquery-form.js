39766cb4bb39eaac05f3cda30c4e262745545a88
Nick Meccia <nickmeccia@gmail.com>
2011-05-30 16:23:38 -0500

    added link to comments

--- public/javascripts/jquery-form.js
+++ public/javascripts/jquery-form.js
@@ -4,6 +4,7 @@
  * @requires jQuery v1.3.2 or later
  *
  * Examples and documentation at: http://malsup.com/jquery/form/
+ * http://www.williambharding.com/blog/rails/rails-ajax-image-uploading-made-simple-with-jquery/
  * Dual licensed under the MIT and GPL licenses:
  *   http://www.opensource.org/licenses/mit-license.php
  *   http://www.gnu.org/licenses/gpl.html
